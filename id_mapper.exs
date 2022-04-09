20ca45e8e147c39296aa6afffd74894b3d4c4ce1
okkdev <dev@stehlik.me>
2022-04-09 14:59:29 +0200

    update id mapper

--- id_mapper.exs
+++ id_mapper.exs
@@ -5,74 +5,65 @@ Mix.install([
 ])
 
 defmodule IdMapper do
-  def gen_card_json(md_cards) do
-    md_cards
+  def run do
+    :cards = :ets.new(:cards, [:set, :protected, :named_table, read_concurrency: true])
+
+    cardnum =
+      Req.get!("https://www.masterduelmeta.com/api/v1/cards?collectionCount=true").body
+      |> String.to_integer()
+
+    pages =
+      (cardnum / 3000)
+      |> ceil()
+
+    1..pages
+    |> Enum.flat_map(fn page ->
+      url =
+        "https://www.masterduelmeta.com/api/v1/cards?limit=3000&page=#{page}"
+        |> URI.encode()
+
+      Req.get!(url).body
+    end)
+    |> Enum.filter(&(&1["alternateArt"] != true))
+    |> Enum.map(
+      &{
+        normalize_string(&1["name"]),
+        &1["konamiID"]
+      }
+    )
+    |> tap(&Owl.ProgressBar.start(id: :bar, label: "Progress:", total: length(&1)))
+    |> then(&:ets.insert(:cards, &1))
+
+    "md_cards.txt"
     |> File.read!()
     |> String.split("\n")
     |> Enum.map(&String.split(&1, ": ", parts: 2))
-    |> tap(&Owl.ProgressBar.start(id: :bar, label: "Progress:", total: length(&1)))
-    |> tap(fn cards ->
-      Owl.LiveScreen.add_block(
-        :status,
-        state: :init,
-        render: fn
-          :init ->
-            "..."
-
-          :done ->
-            Owl.Data.tag("Done!", :green)
-
-          :writing ->
-            Owl.Data.tag("Writing to file...", :yellow)
-
-          {:mapping, name, num} ->
-            [
-              "Mapping: ",
-              Owl.Data.tag(name, :yellow),
-              "\n",
-              "Status: ",
-              Owl.Data.tag("#{num}/#{length(cards)}", :yellow)
-            ]
-        end
-      )
-    end)
-    |> Enum.with_index(1)
-    |> Enum.map(fn {[id, name], index} ->
-      # sleep to avoid getting ratelimited
-      Process.sleep(60)
+    |> Enum.map(fn [id, name] ->
       Owl.ProgressBar.inc(id: :bar)
-      Owl.LiveScreen.update(:status, {:mapping, name, index})
+
+      kid =
+        case :ets.lookup(:cards, normalize_string(name)) do
+          [e | _] -> e |> elem(1) |> String.to_integer()
+          _ -> nil
+        end
 
       %{
         id: String.to_integer(id),
-        konami_id: fetch_id(name),
+        konami_id: kid,
         name: name
       }
     end)
     |> Jason.encode!()
-    |> tap(fn _ -> Owl.LiveScreen.update(:status, :writing) end)
     |> then(&File.write!("mapped_cards.json", &1))
 
-    Owl.LiveScreen.update(:status, :done)
     Owl.LiveScreen.await_render()
   end
 
-  defp fetch_id(name, fuzzy \\ false) do
-    "https://db.ygoprodeck.com/api/v7/cardinfo.php?#{if fuzzy, do: "fname", else: "name"}=#{name}"
-    |> URI.encode()
-    |> Req.get!()
-    |> case do
-      %{body: %{"data" => [card | _]}} ->
-        card["id"]
-
-      _ ->
-        if fuzzy do
-          nil
-        else
-          fetch_id(name, true)
-        end
-    end
+  defp normalize_string(string) when is_binary(string) do
+    string
+    |> String.downcase()
+    |> String.replace(~r/[^a-z0-9.]/, "")
   end
 end
 
-IdMapper.gen_card_json("md_cards.txt")
+IdMapper.run()
